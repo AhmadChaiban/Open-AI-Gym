428bc319408b1dab0a227ea2b0984d82d28d517b
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-10 15:07:26 +0200

    Feat (tf 2.0 policy gradients)
    Began work on building the policy gradients method in tensorflow 2.0

--- CartPole/self_pg_tf.py
+++ CartPole/self_pg_tf.py
@@ -6,9 +6,92 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from gym import wrappers
 from datetime import datetime
-from self_q_learning_bins import plot_running_avg
+from self_q_learning import plot_running_avg, play_one
 
-## Creating an object to test different architectures
-class HiddenLayer:
-    def __init__(self, M1, M2, f = tf.nn.tanh, use_bias = True):
-        self.W = tf.Variable(tf.random_normal(shape = M1, M2))
+## Model object
+class Model:
+    def __init__(self, input_dim, dense1, dense2, lr, from_logits):
+        ## Defining the model
+        self.model = tf.keras.sequential()
+        self.model.add(tf.keras.layers.Dense(dense1, input_dim = input_dim, activation = 'relu'))
+        self.model.add(tf.keras.layers.Dense(dense2, activation = 'softmax'))
+        self.model.build()
+        ## Defining the optimizer and loss function
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate = lr)
+        self.compute_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits= from_logits)
+        ## Variable to hold gradients
+        self.gradBuffer = self.model.trainable_variables
+        for index,grad in enumerate(self.gradBuffer):
+            self.gradBuffer[index] = grad*0
+
+    def discount_rewards(self, rewards, gamma=0.8):
+        # gamma: discount rate
+        discounted_rewards = np.zeros_like(rewards)
+        running_add = 0
+        for t in reversed(range(0, rewards.size)):
+            running_add = running_add * gamma + rewards[t]
+            discounted_rewards[t] = running_add
+        return discounted_rewards
+
+    def get_action(self, logits):
+        a_dist = logits.numpy()
+        # Choose random action with p = action dist
+        action = np.random.choice(a_dist[0],p=a_dist[0])
+        action = np.argmax(a_dist == action)
+        return action
+
+    def apply_gradient(self, ep_memory):
+        for grads, r in ep_memory:
+            for ix,grad in enumerate(grads):
+                self.gradBuffer[ix] += grad * r
+
+
+    def play_one_episode(self, env, scores, ep_score, ep_memory):
+            ## reset the environment
+            env = env.reset()
+            done = False
+            while not done:
+                observation = env.T
+                with tf.GradientTape() as tape:
+                    #forward pass
+                    logits = self.model(observation)
+                    action = self.get_action(logits)
+                    loss = self.compute_loss([action], logits)
+                grads = tape.gradient(loss, self.model.trainable_variables)
+                # make the choosen action
+                observation, reward, done, _ = env.step(action)
+                ep_score += reward
+                if done:
+                    reward -= 10 # small trick to make training faster
+                ep_memory.append([grads,reward])
+            #Discound the rewards
+            ep_memory = np.array(ep_memory)
+            ep_memory[:,1] = self.discount_rewards(ep_memory[:,1])
+
+            self.apply_gradient(self, ep_memory)
+
+            if e % update_every == 0:
+                optimizer.apply_gradients(zip(self.gradBuffer, self.model.trainable_variables))
+                for ix,grad in enumerate(self.gradBuffer):
+                    self.gradBuffer[ix] = grad * 0
+
+            if e % 100 == 0:
+                print("Episode  {}  Score  {}".format(e, np.mean(scores[-100:])))
+
+if __name__ == '__main__':
+    gym.envs.register(
+        id='Cart-v0',
+        entry_point='gym.envs.classic_control:CartPoleEnv',
+        max_episode_steps=10000
+        #reward_threshold=-110.0,
+    )
+    env = gym.make('Cart-v0')
+    N = 10000
+    scores = []
+    for e in range(N):
+        ep_memory = []
+        ep_score = 0
+
+
+
+        scores.append(ep_score)
