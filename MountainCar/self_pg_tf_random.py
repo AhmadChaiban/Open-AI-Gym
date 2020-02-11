526ffbfa53c508062fb9184131f4dd8387a78a1f
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-11 17:22:59 +0200

    Feat (Began model building)
    -- Began building the neural network with tf 2.0 
    -- May have a hint on solving the continuous problem

--- MountainCar/self_pg_tf_random.py
+++ MountainCar/self_pg_tf_random.py
@@ -1,4 +1,19 @@
 import gym
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
 import matplotlib.pyplot as plt
+
+## Need a gaussian activation function 
+class Model:
+    def __init__ (self, input, dense1, dense2):
+        self.model = Sequential([
+            Dense(dense1, input_dim = input, )
+        ])
+
+
+
+if __name__ == '__main__':
+    env = gym.make('MountainCarContinuous-v0')
+
