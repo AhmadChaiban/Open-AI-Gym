33437cde9a632e536ee21fb385d463fdf1228f58
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-13 21:47:27 +0200

    Feat (play one and multiple episodes)
    Built functions that allow for playing multiple episodes, where that also has a smaller function within that plays one episode. 
    
    There is also a progress bar being implemented for feedback, and environment rendering for the game

--- CartPole/dqn_tf.py
+++ CartPole/dqn_tf.py
@@ -83,24 +83,63 @@ class LearningAgent:
             ## Fit the network on the state, target for n epochs
             self.q_network.fit(state, target, epochs=1, verbose=0)
 
+    def play_one_episode(self, env, state, bar, agent, timesteps_per_episode, batch_size):
+        for timestep in range(timesteps_per_episode):
+            # Run Action
+            action = agent.act(state)
+            # Take action
+            next_state, reward, terminated, info = env.step(action)
+            next_state = np.reshape(next_state, [1, 1])
+            agent.store(state, action, reward, next_state, terminated)
+            ## Flip states
+            state = next_state
+            ## Termination check
+            if terminated:
+                agent.alighn_target_model()
+                break
+            ## refer to experience replay for retraining
+            if len(agent.experience_replay) > batch_size:
+                agent.retrain(batch_size)
+            ## update the bar
+            if timestep%10 == 0:
+                bar.update(timestep/10 + 1)
 
-
-
-
-
-
-
-
-
-
-
+    def play_multiple_episodes(self, env, agent, num_episodes, bar, timesteps_per_episode, batch_size):
+        for e in range(0, num_episodes):
+            ## reset the environment
+            state = env.reset()
+            state = np.reshape(state, [1, 1])
+            ## initialize the variables
+            reward = 0
+            terminated = False
+            self.play_one_episode(env, state, bar, agent, timesteps_per_episode, batch_size)
+            bar.finish()
+            if (e + 1) % 10 == 0:
+                print("**********************************")
+            print("Episode: {}".format(e + 1))
+            env.render()
+            print("**********************************")
 
 
 if __name__ == '__main__':
+    ## Creating the Taxi v2 environment
     env = gym.make("Taxi-v2").env
-    env.render()
-
-    print(f'Number of states: {env.observation_space.n}')
-    print(f'Number of actions: {env.action_space.n}')
+    ## Defining the adam optimizer with the learning rate
+    optimizer = Adam(learning_rate=0.01)
+    ## Creating a learning agent using the environment and the optimizer
+    agent_7bb_booty = LearningAgent(env, optimizer)
+    ## Define some basic parameters
+    batch_size = 32
+    num_episodes = 100
+    timesteps_per_episode = 1000
+    ## Give a summary of the DQN
+    agent_7bb_booty.q_network.summary()
+    ## defining the progress bar
+    bar = progressbar.ProgressBar(maxval=timesteps_per_episode/10, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+    bar.start()
+    ## Let the agent play! 7bb play bibibibi play!
+    agent_7bb_booty.play_multiple_episodes(env, agent_7bb_booty, num_episodes, bar, timesteps_per_episode, batch_size)
+    # print(f'Number of states: {env.observation_space.n}')
+    # print(f'Number of actions: {env.action_space.n}')
 
 
