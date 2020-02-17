5836e79fafe81d65d62aca7cc062adc07d0f121e
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-17 22:11:14 +0200

    Feat (self_master)
    -- Added a master file that contains all the info about the master Policy/Value networks
    
    -- Added the master agent. It holds a shared optimizer that updates its global network. This agent instantiates the global network that each worker agent will update as well as the optimizer that we will use to update it.

---
+++ a3c/self_master.py
@@ -0,0 +1,17 @@
+import numpy as np
+import gym
+import tensorflow as tf
+from tensorflow.keras import optimizers
+
+class MasterAgent:
+    def __init__(self):
+        self.game_name = 'CartPole-v0'
+
+        env = gym.make(self.game_name)
+        self.state_size = env.observation_space.shape[0]
+        self.action_size = env.action_space.n
+        self.optimizer = optimizers.Adam(learning_rate = 0.01)
+        print(self.state_size, self.action_size)
+
+        self.global_model = ActorCriticModel(self.state_size, self.action_size)
+        self.global_model(tf.convert_to_tensor(np.random.random((1, self.state_size)), dtype=tf.float32))
