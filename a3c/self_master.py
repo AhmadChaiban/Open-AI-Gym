0e863a8204410b97f1b611a58c1320fda92cd543
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-17 22:26:21 +0200

    Refactor (State - Action Size display)
    -- Refactored the state size and action size printing in the MasterAgent class

--- a3c/self_master.py
+++ a3c/self_master.py
@@ -11,7 +11,9 @@ class MasterAgent:
         self.state_size = env.observation_space.shape[0]
         self.action_size = env.action_space.n
         self.optimizer = optimizers.Adam(learning_rate = 0.01)
-        print(self.state_size, self.action_size)
+        print(f"State Size {self.state_size} Action Size {self.action_size}")
 
         self.global_model = ActorCriticModel(self.state_size, self.action_size)
         self.global_model(tf.convert_to_tensor(np.random.random((1, self.state_size)), dtype=tf.float32))
+
+
