66635c9ed9f77aee6d5bd6ef26ea4ad802cda46a
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-18 03:22:48 +0200

    Feat (play function)
    -- Function added to test the Master model

--- a3c/self_master.py
+++ a3c/self_master.py
@@ -7,6 +7,7 @@ from self_random_agent import RandomAgent
 import multiprocessing
 import matplotlib.pyplot as plt
 from self_worker import Worker
+import os
 
 class MasterAgent:
     def __init__(self):
@@ -63,6 +64,32 @@ class MasterAgent:
         [w.join() for w in workers]
 
         self.plot_moving_average_rewards(moving_average_rewards)
+
+    def play(self):
+        env = gym.make(self.game_name).unwrapped
+        state = env.reset()
+        model = self.global_model
+        model_path = os.path.join(self.save_dir, 'model_{}.h5'.format(self.game_name))
+        print('Loading model from: {}'.format(model_path))
+        model.load_weights(model_path)
+        done = False
+        step_counter = 0
+        reward_sum = 0
+
+        try:
+            while not done:
+                env.render(mode='rgb_array')
+                policy, value = model(tf.convert_to_tensor(state[None, :], dtype=tf.float32))
+                policy = tf.nn.softmax(policy)
+                action = np.argmax(policy)
+                state, reward, done, _ = env.step(action)
+                reward_sum += reward
+                print("{}. Reward: {}, action: {}".format(step_counter, reward_sum, action))
+                step_counter += 1
+        except KeyboardInterrupt:
+            print("Received Keyboard Interrupt. Shutting down.")
+        finally:
+            env.close()
 
 
 
