ddbd9bd84b21f73dcd82dbd3c1dfd95d5891fb32
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-18 02:01:47 +0200

    Feat (Memory Class)
    -- A class that will help keep track of the states, actions and rewards which occur per step. Just contains simple functions that append the states, actions and rewards to lists or clear them.

--- a3c/self_networks.py
+++ a3c/self_networks.py
@@ -21,3 +21,18 @@ class ActorCriticModel(keras.Model):
         values = self.values(v1)
         return logits, values
 
+class Memory:
+    def __init__(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
+
+    def store(self, state, action, reward):
+        self.states.append(state)
+        self.actions.append(action)
+        self.rewards.append(reward)
+
+    def clear(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
