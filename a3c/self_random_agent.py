00b26411741208c1a65ef67ef6c5d84a9dca7e6e
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-18 03:10:27 +0200

    Feat (record function)
    Added a function that allows for recording certain statistics for the model

--- a3c/self_random_agent.py
+++ a3c/self_random_agent.py
@@ -1,7 +1,5 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from queue import Queue
+import gym
 
 
 ## This is the random agent that will play the specified game
@@ -38,3 +36,25 @@ class RandomAgent:
                                                    self.res_queue, 0, steps)
 
         return reward_sum
+
+def record(episode,
+           episode_reward,
+           worker_idx,
+           global_ep_reward,
+           result_queue,
+           total_loss,
+           num_steps):
+    if global_ep_reward == 0:
+        global_ep_reward = episode_reward
+    else:
+        global_ep_reward = global_ep_reward * 0.99 + episode_reward * 0.01
+    print(
+        f"Episode: {episode} | "
+        f"Moving Average Reward: {int(global_ep_reward)} | "
+        f"Episode Reward: {int(episode_reward)} | "
+        f"Loss: {int(total_loss / float(num_steps) * 1000) / 1000} | "
+        f"Steps: {num_steps} | "
+        f"Worker: {worker_idx}"
+    )
+    result_queue.put(global_ep_reward)
+    return global_ep_reward
